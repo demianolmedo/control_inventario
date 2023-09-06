ebbcb6773cd6480fb15d5eba8bd7c36ac218d3e7
DEMIAN OLMEDO CADIMA <117462161+demianolmedo@users.noreply.github.com>
2023-09-06 15:57:54 -0400

    Update sgm.js

--- BACKEND/config/sgm.js
+++ BACKEND/config/sgm.js
@@ -1,10 +1,10 @@
 const mariadb = require('mariadb');
 const pool = mariadb.createPool({
-     host: '89.117.59.147', 
-     user:'datacombo', 
-     password: 'D4t4comiano$2023!',
+     host: 'HOST', 
+     user:'USER', 
+     password: 'PASS',
      charset: 'utf8mb4',
-     database: 'bd_SGM',
+     database: 'bd_NOMBRE',
      operatorsAliases: 0     
      //connectionLimit: 15
 });
